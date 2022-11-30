b1f2f6c2909cd1f6db261f45ede51868e0ce2230
Malibu <malibu090@mail.ru>
2022-11-30 07:26:59 +0300

    Разные решения задач

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int randomDigit = new Random().Next(10, 100); //случайное число от 10 до 99 (если в коцне поставить"]" то будет до 100)
+Console.WriteLine("Случайное число " +randomDigit);
+
+int ostatok = randomDigit%10; // остаток от деления на 10
+Console.WriteLine("Последняя цифра " +ostatok);
+
+int firstDigit = randomDigit / 10; //первая цифра от деления на 10
+Console.WriteLine("Первая цифра " +firstDigit);
+
+int max = firstDigit; //задаем переменную макс равную первой цифре
+
+if (ostatok > firstDigit) // проверяем условие, первая ли цифра максимальная?
+    max = ostatok; // если нет, то объявляем вторую максимальной
+
+Console.WriteLine("Максимальная цифра " +max);
---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.Clear();
+int randomDigit = new Random().Next(100, 1000); // Случайное трехзначное число
+Console.WriteLine("Случайное трехзначное число " + randomDigit);
+
+// int firstDigit = randomDigit / 100; // первая цифра
+// Console.WriteLine("первая цифра числа " + firstDigit);
+
+// int lastDigit = randomDigit % 10; // последняя цифра
+// Console.WriteLine("последняя цифра числа " + lastDigit);
+
+// Console.WriteLine("Число без второй цифры " + firstDigit + lastDigit);
+
+int oneDigit = (randomDigit / 100) * 10 + randomDigit % 10; // выводит сумму первой и последней цифры
+Console.WriteLine(randomDigit + " -> " + oneDigit);
+
